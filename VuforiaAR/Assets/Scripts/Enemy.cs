44218ee2aaf169e3ea66088d11c05b5d9fb777d1
KID <50666207+KID421@users.noreply.github.com>
2020-06-11 16:17:31 +0800

    完成版

---
+++ VuforiaAR/Assets/Scripts/Enemy.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("速度"), Range(0, 10)]
+    public float speed = 1.5f;
+    [Header("停止距離"), Range(0, 5)]
+    public float stopDistance = 2;
+    [Header("追蹤目標名稱")]
+    public string targetName = "塔";
+    [Header("爆炸效果")]
+    public GameObject explosion;
+    [Header("傷害"), Range(10, 100)]
+    public float damage;
+
+    private Transform target;
+    private NavMeshAgent nma;
+
+    private void Start()
+    {
+        nma = GetComponent<NavMeshAgent>();
+        nma.speed = speed;
+        nma.stoppingDistance = stopDistance;
+
+        target = GameObject.Find(targetName).transform;
+        nma.SetDestination(target.position);
+    }
+
+    private void Update()
+    {
+        Track();
+    }
+
+    private void OnMouseDown()
+    {
+        ClickAndDead();
+    }
+
+    private void ClickAndDead()
+    {
+        Tower.target = transform;
+        CreateEffect();
+    }
+
+    private void Track()
+    {
+        nma.SetDestination(target.position);
+
+        if (nma.remainingDistance <= stopDistance)
+        {
+            CreateEffect();
+            target.GetComponent<Tower>().Damage(damage);
+        }
+    }
+
+    private void CreateEffect()
+    {
+        GameObject expl = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(expl, 0.5f);
+        Destroy(gameObject);
+    }
+}
---
+++ VuforiaAR/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [Header("要生成的敵人")]
+    public GameObject enemy;
+    [Header("要生成的位置")]
+    public Transform[] points;
+    [Header("一開始的間隔時間")]
+    public float time = 2.5f;
+    [Header("每次間隔減少時間")]
+    public float timeSub = 0.05f;
+
+    public void Spawn()
+    {
+        int r = Random.Range(0, points.Length);
+        Instantiate(enemy, points[r]);
+
+        Invoke("Spawn", time);
+        time -= timeSub;
+        time = Mathf.Clamp(time, 0.5f, 2.5f);
+    }
+}
